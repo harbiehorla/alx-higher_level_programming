f88a1ae3e539b3f5a3e2e5f5d927bd5484339b56
harbiehorla <harbiehorla@gmail.com>
2023-11-30 11:08:59 -0800

    error fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     userin = argv[1:]
     size = len(userin)
     print("{:d} {:s}{:s}".
-          format(size,
-                 "arguments" if (size) is not 1 else "argument",
-                 "." if (size) is 0 else ":"))
-    for idx, arg in enumerate(userin):
-        print("{:d}: {:s}".format(idx + 1, arg))
+            format(size,
+                "arguments" if (size) is not 1 else "argument",
+                "." if (size) is 0 else ":"))
+            for idx, arg in enumerate(userin):
+                print("{:d}: {:s}".format(idx + 1, arg))
