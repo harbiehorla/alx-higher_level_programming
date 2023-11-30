ef66058dbb703cef081e2ff716f66cbc5c7e014a
harbiehorla <harbiehorla@gmail.com>
2023-11-30 11:30:28 -0800

    error fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-from sys import argv
 
+# 2_argv.py
 if __name__ == "__main__":
-    length = len(argv) - 1
-    print(f"{length} {'argument' if length == 1 else 'arguments'}{'.' if length == 0 else ':'}")
-    for i in range(1, length + 1):
-        print(f"{i}: {argv[i]}")
+    """Program that prints the number of and the list of its arguments."""
+
+    from sys import argv
+
+    count = len(argv)
+    if (count-1) == 1:
+        print("{} argument".format(count - 1), end="")
+    else:
+        print("{} arguments".format(count - 1), end="")
+
+    if (count - 1) == 0:
+        print(".")
+    else:
+        print(":")
+
+    for i in range(1, count):
+        print("{}: {}".format(i, argv[i]))
