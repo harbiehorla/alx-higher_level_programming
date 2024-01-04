ae1360ca68517b7585546b5a5928cd61531dfb47
harbiehorla <harbiehorla@gmail.com>
2024-01-04 12:49:03 -0800

    0x8-python-more-classes

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -43,6 +43,7 @@ class Rectangle:
         """Get/set the height of the Rectangle."""
         return self.__height
     @height.setter
+
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
